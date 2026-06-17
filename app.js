03ae5d58566aa40517b08ef32ce430fbd1c94839
Saraththecoder <sarathkumar86518@gmail.com>
2026-06-18 01:37:31 +0530

    feat: remove YouTube watch button from awareness stories

--- app.js
+++ app.js
@@ -1220,25 +1220,6 @@ function renderStory() {
   } else {
     dom.storyArt.innerHTML = `<img src="./story${storyIndex}.png" class="story-art-img" alt="${activeStory.title}">`;
   }
-  // Render or update the Watch Video button
-  let ytBtn = document.getElementById('story-yt-btn');
-  if (!ytBtn) {
-    ytBtn = document.createElement('a');
-    ytBtn.id = 'story-yt-btn';
-    ytBtn.className = 'story-yt-btn';
-    ytBtn.target = '_blank';
-    ytBtn.rel = 'noopener noreferrer';
-    // Insert before the story nav
-    const nav = document.querySelector('.story-nav');
-    if (nav) nav.parentNode.insertBefore(ytBtn, nav);
-  }
-  ytBtn.href = activeStory.ytLink;
-  ytBtn.innerHTML = `
-    <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-      <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-    </svg>
-    <span>Watch on YouTube</span>
-  `;
   
   // Enable/disable buttons
   dom.storyPrevBtn.style.visibility = appState.currentStoryIndex === 0 ? 'hidden' : 'visible';
